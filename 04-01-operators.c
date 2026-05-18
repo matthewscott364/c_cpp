b161058287081df52b360d097df84845f0d4cc0e
Matthew Scott <matthewscott364@gmail.com>
2026-05-18 18:59:29 -0400

    started and finished operators

---
+++ 04-01-operators.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int x = 10;
+    int y = 2;
+    int z = 0;
+
+    // z = x + y;
+    // z = x - y;
+    // z = x * y;
+    // z = x / y;
+    // z = x % y;
+
+    x+=3;
+
+    printf("%d", x);
+
+    return 0;
+}
