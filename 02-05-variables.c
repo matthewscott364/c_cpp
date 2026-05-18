8d58dce516516138c49932266132e2b485ec9c7c
Matthew Scott <matthewscott364@gmail.com>
2026-05-18 17:17:10 -0400

    new c files

--- 02-05-variables.c
+++ 02-05-variables.c
@@ -3,6 +3,8 @@
 int main(){
     char name[] = "Matthew Scott";
     char food[] = "chicken";
+    char cpp[] = "C++";
     printf("Hello, %s.\n", name);
     printf("Your favorite food is %s!\n", food);
+    printf("Eventually I will also learn %s\n", cpp);
 }
