8fdd40a552ac5f465e6067b2ee79e718642f3b78
Matthew Scott <matthewscott364@gmail.com>
2026-05-19 15:38:23 -0400

    7-2

---
+++ 07-02-ifstatements.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    bool isStudent = true;
+    if(isStudent == true) {
+        printf("You are a student");
+    }
+    else {
+        printf("You are not a student");
+    }
+}
