7e5ff065f319b0fc418ac08577c906b443f16ec6
Matthew Scott <matthewscott364@gmail.com>
2026-05-20 09:45:25 -0400

    return

--- c-12-01-returns.c
+++ c-12-01-returns.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+double square(double num) {
+    return num * num;
+}
+
 int main () {
-    
+    double x = square(2.1);
+    double y = square(3.2);
+    double z = square(4.3);
+
+    printf("%lf\n", x);
+    printf("%lf\n", y);
+    printf("%lf\n", z);
 }
