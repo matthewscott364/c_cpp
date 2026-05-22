4fbcb14e801da769e401342a0d3a35a960c00b96
Matthew Scott <matthewscott364@gmail.com>
2026-05-22 15:08:36 -0400

    16-01-inc

---
+++ c-16-01-forloops.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// For Loop = Repeat some code a limited # of times
+// for(Initialization; Condition; Update)
+
+int main() {
+    for(int i = 10; i >= 0; i--) {
+        printf("%d\n", i);
+    }
+    return 0;
+};
