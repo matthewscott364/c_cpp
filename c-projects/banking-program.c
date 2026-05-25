40aa2b239eb187d07bb87b7a5eb53a794dcaf8d2
Matthew Scott <matthewscott364@gmail.com>
2026-05-25 03:10:09 -0400

    forgot to save

---
+++ c-projects/banking-program.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void checkBalance(float balance);
+float deposit();
+float withdraw(float balance);
+
+int main() {
+    return 0;
+}
+
+void checkBalance(float balance) {
+}
+float deposit() {
+    return 0.0f;
+}
+float withdraw(float balance) {
+    return 0.0f;
+}
