e338287bb7117b572507bb87ff978b3390d945ad
Matthew Scott <matthewscott364@gmail.com>
2026-05-29 10:53:28 -0400

    stringarrays-3

--- c-23-03-string-arrays.c
+++ c-23-03-string-arrays.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-    char fruits[][10] = {"Apple", "Banana", "Cononut"};
-    int size = sizeof(fruits) / sizeof(fruits[0]);
+    char names[4][25] = {0};
+    int rows = sizeof(names) / sizeof(names[0]);
 
-    fruits[0][0] = 'e';
-    fruits[0][4] = 'A';
-
-    fruits[1][0] = 'a';
-    fruits[1][5] = 'B';
-
-    fruits[2][0] = 't';
-    fruits[2][6] = 'C';
-
-    for(int i = 0; i < size; i++) {
-        printf("%s\n", fruits[i]);
+    for(int i = 0; i < rows; i++) {
+        printf("Enter a name: ");
+        fgets(names[i], sizeof(names[i]), stdin);
+        names[i][strlen(names[i]) - 1] = '\0';
+    }
+    for(int i = 0; i < rows; i++) {
+         printf("%s\n", names[i]);
     }
     return 0;
 }
