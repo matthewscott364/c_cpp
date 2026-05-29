70079707a518b68880b85bd1394947e50a9f4c77
Matthew Scott <matthewscott364@gmail.com>
2026-05-29 11:07:31 -0400

    ternary op 1-2

---
+++ c-24-01-ternary-operator.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int x = 5;
+    int y = 6;
+    int max = (x > y) ? x : y;
+
+    printf("%d", max);
+    return 0;
+}
---
+++ c-24-02-ternary-operator.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    bool isOnline = true;
+    printf("%s", (isOnline) ? "online" : "offline");
+    return 0;
+}
